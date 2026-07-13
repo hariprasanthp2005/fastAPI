3f50a5bb85882ff6d1af0820f98b3c68ed98e271
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-07-13 21:53:52 +0530

    fastAPI

---
+++ fastone1/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app=FastAPI()
+
+'''
+Usage: Application Root Request
+Rest API URL: http://127.0.0.1:8000/
+Method Type:GET
+Required Fields: None 
+Acccess Type:Public
+'''
+@app.post("/")
+def index_page():
+    return {"msg":"Index Page"}
