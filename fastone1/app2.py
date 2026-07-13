cca21e4aabdca368b57236376ee8ef900ce67b93
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-07-13 22:07:06 +0530

    fastAPI

---
+++ fastone1/app2.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+students = {
+    1: {"name": "Hari", "age": 21},
+    2: {"name": "Ravi", "age": 22}
+}
+@app.get("/students")
+def get_students():
+    return students
+
+@app.post("/students/{student_id}")
+def add_student(student_id: int, name: str, age: int):
+    students[student_id] = {
+        "name": name,
+        "age": age
+    }
+
+    return {
+        "message": "Student added successfully"
+    }
+
