86ecea8e2845fe0e6b958422e39e777a0b044866
Hari Prasanth <hariprasanthp2005@gmail.com>
2026-07-23 09:51:09 +0530

    fastapi

--- path_eg1/routes/product_eg.py
+++ path_eg1/routes/product_eg.py
@@ -4,9 +4,27 @@ router = APIRouter(prefix="/products")
 
 products = [
     {"id": 101, "name": "HP Victus", "brand": "HP", "category": "Laptop"},
-    {"id": 102, "name": "Dell Inspiron", "brand": "Dell", "category": "Laptop"},
+    {"id": 102, "name": "Dell Inspiron", "brand": "dell", "category": "Laptop"},
     {"id": 103, "name": "iPhone 16", "brand": "Apple", "category": "Mobile"},
 ]
+@router.get("/")
+def get_products(category: str):
+    return [
+        p for p in products
+        if p["category"].lower() == category.lower()
+    ]
+
+
+@router.get("/read")
+def get_product(category: str | None = None):
+    print(category)
+    if category is None:
+        return products
+
+    return [
+        p for p in products
+        if p["category"].lower() == category.lower()
+    ]
 
 @router.get("/{product_id}")
 def get_product(product_id: int):
@@ -16,9 +34,5 @@ def get_product(product_id: int):
 
     return {"message": "Product not found"}
 
-@router.get("/")
-def get_products(category: str):
-    return [
-        p for p in products
-        if p["category"].lower() == category.lower()
-    ]
+
+
